75467ca116b8ab119793f77bd05ed0e7d86c14c5
عبد المولى أحمد <ahmedabdelmoula1@gmail.com>
2021-04-30 23:40:11 +0100

    fixing stuff

--- src/Components/EditTask.js
+++ src/Components/EditTask.js
@@ -1,28 +1,30 @@
-import React, { useState } from "react";
-import Modal from "react-modal";
-import { useDispatch } from "react-redux";
-import { editTask } from "../redux/actions";
+import React, {useState} from 'react';
+import Modal from 'react-modal';
+import {useDispatch} from 'react-redux';
+import {editTask} from '../redux/actions';
 
 const customStyles = {
   content: {
-    top: "50%",
-    left: "50%",
-    right: "auto",
-    bottom: "auto",
-    marginRight: "-50%",
-    transform: "translate(-50%, -50%)",
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+    borderRadius: '20px',
+    // backgroundColor: 'lightblue',
   },
 };
-Modal.setAppElement("#root");
+Modal.setAppElement('#root');
 
-const EditTask = ({ oldTask }) => {
+const EditTask = ({oldTask}) => {
   const [modalIsOpen, setIsOpen] = useState(false);
   const [newAction, setNewAction] = useState(oldTask.action);
   const dispatch = useDispatch();
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const editedTask = { ...oldTask, action: newAction };
+    const editedTask = {...oldTask, action: newAction};
     dispatch(editTask(editedTask));
     closeModal();
   };
@@ -38,6 +40,7 @@ const EditTask = ({ oldTask }) => {
     <div>
       <button onClick={openModal}>Edit</button>
       <Modal
+        id="mdl"
         isOpen={modalIsOpen}
         onRequestClose={closeModal}
         style={customStyles}
